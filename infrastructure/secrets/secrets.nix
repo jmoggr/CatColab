81f3a04b8002aaa5cef685c0250dfe56275a46aa
Jason Moggridge <github_hou1@jmoggr.com>
2025-03-14 09:26:59 -0400

    Setup to allow Jason testing deployments on separate AWS instance

--- infrastructure/secrets/secrets.nix
+++ infrastructure/secrets/secrets.nix
@@ -1,14 +1,25 @@
-
 let
-  catcolab      = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPyxORhhfO+9F2hQZ3I/EiSpfg+caWpG6c8AuG5u1XtK root@ip-172-31-14-38.us-east-2.compute.internal";
-  catcolab-next = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBZaycYjvaZ5XhxVIvFr8zXvcy1GrViCKLIZCalZuk1l root@ip-172-31-9-45.us-east-2.compute.internal";
-  owen          = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF2sBTuqGoEXRWpBRqTBwZZPDdLGGJ0GQcuX5dfIZKb4 o@red-special";
-  epatters      = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAKXx6wMJSeYKCHNmbyR803RQ72uto9uYsHhAPPWNl2D evan@epatters.org";
-  jmoggr        = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMiaHaeJ5PQL0mka/lY1yGXIs/bDK85uY1O3mLySnwHd j@jmoggr.com";
+  catcolab = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPyxORhhfO+9F2hQZ3I/EiSpfg+caWpG6c8AuG5u1XtK root@ip-172-31-14-38.us-east-2.compute.internal";
+  catcolab-next = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH9P3t2Y6k5Jrwbd2XRyV0ok0BHmyHFfL+E62Qu/pVm+ root@ip-172-31-80-32.ec2.internal";
+  owen = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF2sBTuqGoEXRWpBRqTBwZZPDdLGGJ0GQcuX5dfIZKb4 o@red-special";
+  epatters = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAKXx6wMJSeYKCHNmbyR803RQ72uto9uYsHhAPPWNl2D evan@epatters.org";
+  jmoggr = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMiaHaeJ5PQL0mka/lY1yGXIs/bDK85uY1O3mLySnwHd j@jmoggr.com";
 in
-builtins.mapAttrs (_: publicKeys: {inherit publicKeys;})
-  ({
-      ".env.next.age"   = [ catcolab-next owen epatters jmoggr ];
-      ".env.prod.age"   = [ catcolab owen epatters ];
-      "rclone.conf.age" = [ catcolab owen epatters ];
-  })
+builtins.mapAttrs (_: publicKeys: { inherit publicKeys; }) ({
+  ".env.next.age" = [
+    catcolab-next
+    owen
+    epatters
+    jmoggr
+  ];
+  ".env.prod.age" = [
+    catcolab
+    owen
+    epatters
+  ];
+  "rclone.conf.age" = [
+    catcolab
+    owen
+    epatters
+  ];
+})

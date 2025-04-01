e8cbe92dbc36fbb979958d07ab6fbebdee843128
Jason Moggridge <github_hou1@jmoggr.com>
2025-04-01 09:21:25 -0400

    build

--- infrastructure/modules/host.nix
+++ infrastructure/modules/host.nix
@@ -26,6 +26,7 @@ let
 
     git clone https://github.com/jmoggr/CatColab.git catcolab
     cd catcolab
+    git log
     git checkout "$commit_sha"
 
     sudo /run/current-system/sw/bin/nixos-rebuild switch --flake .#catcolab-jmoggr
